c90d385f2d277619deb9175d1fcb498cc1288f25
Jurko <50539591+JurkoDev@users.noreply.github.com>
2025-03-14 20:28:06 +0100

    add a portable git

--- update.bat
+++ update.bat
@@ -1,9 +1,31 @@
 @echo off
 cd /d %~dp0
+
+REM Check if Git is installed
+git --version >nul 2>&1
+IF ERRORLEVEL 1 (
+    IF EXIST PortableGit (
+        echo Portable Git is already extracted.
+        set PATH=%cd%\PortableGit\cmd;%PATH%
+    ) ELSE (
+        IF EXIST PortableGit48.exe (
+            echo Portable Git executable found. Skipping download.
+        ) ELSE (
+            echo Git is not installed. Downloading portable Git...
+            powershell -Command "Invoke-WebRequest -Uri 'https://github.com/git-for-windows/git/releases/download/v2.48.1.windows.1/PortableGit-2.48.1-64-bit.7z.exe' -OutFile 'PortableGit48.exe'"
+        )
+        echo Extracting Git...
+        powershell -Command "Start-Process 'PortableGit48.exe' -ArgumentList '-o%cd%\PortableGit', '-y', '-gm2' -NoNewWindow -Wait"
+        set PATH=%cd%\PortableGit\cmd;%PATH%
+    )
+) ELSE (
+    echo Git is installed.
+)
+
 IF EXIST .git (
     echo Already Setup Git
 ) ELSE (
-    echo Seting up Git
+    echo Setting up Git
     git init .
     git remote add origin "https://github.com/JurkoDev/dark-magic-pack-autoupdate.git"
     git fetch origin
