7ebe220fbcd0d926578c6483d722d075d926c8aa
Weigurde <83884361+Weigurde@users.noreply.github.com>
2025-03-17 20:35:06 -0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -3,17 +3,17 @@ cd /d %~dp0
 
 REM Check if Python is installed
 
-echo Downloading gui.py...
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/JurkoDev/dark-magic-pack-autoupdate/refs/heads/master/gui.py' -OutFile 'gui.py'"
+rem echo Downloading gui.py...
+rem powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/JurkoDev/dark-magic-pack-autoupdate/refs/heads/master/gui.py' -OutFile 'gui.py'"
 
 
 echo Downloading updategit.bat...
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/JurkoDev/dark-magic-pack-autoupdate/refs/heads/master/updategit.bat' -OutFile 'updategit.bat'"
 
 
-echo Downloading gui.exe...
-powershell -Command "Invoke-WebRequest -Uri 'https://github.com/JurkoDev/dark-magic-pack-autoupdate/raw/refs/heads/master/gui.exe' -OutFile 'gui.exe'"
+rem echo Downloading gui.exe...
+rem powershell -Command "Invoke-WebRequest -Uri 'https://github.com/JurkoDev/dark-magic-pack-autoupdate/raw/refs/heads/master/gui.exe' -OutFile 'gui.exe'"
 
 
-echo Executing gui.exe...
-start gui.exe
+rem echo Executing gui.exe...
+rem start gui.exe
